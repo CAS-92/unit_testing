3109597b9892a2a409855821bb7740d50f99ff52
CAS-92 <148983387+CAS-92@users.noreply.github.com>
2024-07-10 14:27:52 +0100

    Create my_script.sh
    script

---
+++ my_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Enter a number and I will check if it is odd or even: " mynumber
+if [ $((mynumber % 2)) -eq 0 ]; then
+  echo "Your number is even"
+else
+  echo "Your number is odd"
+fi
